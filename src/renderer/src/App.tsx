679e2aa8f1702139cd56cf38adbc52a598552cee
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-02-14 11:17:25 -0300

    refactoring: move create product into another component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,6 @@
-import { FormEvent, useRef, useState } from 'react'
+import { FormEvent, useState } from 'react'
 
-type ProductType = {
+export type ProductType = {
   id: string
   name: string
   price: number
@@ -9,41 +9,10 @@ type ProductType = {
 let currentAudio: HTMLAudioElement | null = null
 
 export default function App() {
-  const [productIdInput, setProductIdInput] = useState('')
-  const [productNameInput, setProductNameInput] = useState('')
-  const [productPriceInput, setProductPriceInput] = useState('')
-
   const [searchIdInput, setSearchIdInput] = useState('')
-
   const [product, setProduct] = useState<ProductType | null>(null)
+
   const [isLoading, setIsLoading] = useState<boolean>(false)
-
-  const formRef = useRef<HTMLFormElement>(null)
-
-  async function handleCreateProduct(e: FormEvent) {
-    e.preventDefault()
-    setIsLoading(true)
-
-    if (!productIdInput || !productNameInput || !productPriceInput) return
-
-    const newProduct = {
-      id: productIdInput,
-      name: productNameInput,
-      price: parseFloat(productPriceInput)
-    } as ProductType
-
-    try {
-      await window.api.products.create(newProduct)
-
-      setProductIdInput('')
-      setProductNameInput('')
-      setProductPriceInput('')
-    } catch (error) {
-      window.api.log(JSON.stringify(error))
-    } finally {
-      setIsLoading(false)
-    }
-  }
 
   async function handleFindProductById(e: FormEvent) {
     e.preventDefault()
@@ -89,49 +58,7 @@ export default function App() {
   }
 
   return (
-    <div className="flex items-center justify-center h-screen w-screen gap-8 text-center">
-      <form
-        id="create-product"
-        ref={formRef}
-        onSubmit={handleCreateProduct}
-        className="flex flex-col gap-2 border w-80 h-64 p-4 rounded-lg"
-      >
-        <h3>Criar Produto</h3>
-
-        <div className="flex-1 flex flex-col justify-evenly">
-          <input
-            required
-            placeholder="Nome do produto"
-            className="border px-4 py-2 rounded-lg"
-            type="text"
-            value={productNameInput}
-            onChange={(e) => setProductNameInput(e.target.value)}
-          />
-          <input
-            required
-            placeholder="Preço do produto"
-            className="border px-4 py-2 rounded-lg"
-            type="number"
-            step={0.01}
-            min={0}
-            value={productPriceInput}
-            onChange={(e) => setProductPriceInput(e.target.value)}
-          />
-          <input
-            required
-            placeholder="Código do produto"
-            className="border px-4 py-2 rounded-lg"
-            type="text"
-            value={productIdInput}
-            onChange={(e) => setProductIdInput(e.target.value)}
-          />
-        </div>
-
-        <button disabled={isLoading} className="border px-4 py-2 rounded-lg" type="submit">
-          {isLoading ? <span className="animate-pulse">Carregando</span> : <span>Criar</span>}
-        </button>
-      </form>
-
+    <main className="flex items-center justify-center h-screen w-screen gap-8 text-center">
       <form
         onSubmit={handleFindProductById}
         id="search-product"
@@ -146,13 +73,15 @@ export default function App() {
         />
 
         <div className="flex flex-1 items-center justify-center">
-          <p className="truncate">{`${product?.name} - R$${product?.price}` || '...'}</p>
+          <p className="truncate">
+            {product?.name && product.price ? `${product?.name} - R$${product?.price}` : '...'}
+          </p>
         </div>
 
         <button disabled={isLoading} className="border px-4 py-2 rounded-lg" type="submit">
           {isLoading ? <span className="animate-pulse">Carregando</span> : <span>Buscar</span>}
         </button>
       </form>
-    </div>
+    </main>
   )
 }
---
+++ src/renderer/src/components/create-product.tsx
@@ -0,0 +1,81 @@
+import { ProductType } from '@renderer/App'
+import { FormEvent, useRef, useState } from 'react'
+
+export function CreateProduct() {
+  const [productIdInput, setProductIdInput] = useState('')
+  const [productNameInput, setProductNameInput] = useState('')
+  const [productPriceInput, setProductPriceInput] = useState('')
+
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const formRef = useRef<HTMLFormElement>(null)
+
+  async function handleCreateProduct(e: FormEvent) {
+    e.preventDefault()
+    setIsLoading(true)
+
+    if (!productIdInput || !productNameInput || !productPriceInput) return
+
+    const newProduct = {
+      id: productIdInput,
+      name: productNameInput,
+      price: parseFloat(productPriceInput)
+    } as ProductType
+
+    try {
+      await window.api.products.create(newProduct)
+
+      setProductIdInput('')
+      setProductNameInput('')
+      setProductPriceInput('')
+    } catch (error) {
+      window.api.log(JSON.stringify(error))
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <form
+      id="create-product"
+      ref={formRef}
+      onSubmit={handleCreateProduct}
+      className="flex flex-col gap-2 border w-80 h-64 p-4 rounded-lg"
+    >
+      <h3>Criar Produto</h3>
+
+      <div className="flex-1 flex flex-col justify-evenly">
+        <input
+          required
+          placeholder="Nome do produto"
+          className="border px-4 py-2 rounded-lg"
+          type="text"
+          value={productNameInput}
+          onChange={(e) => setProductNameInput(e.target.value)}
+        />
+        <input
+          required
+          placeholder="Preço do produto"
+          className="border px-4 py-2 rounded-lg"
+          type="number"
+          step={0.01}
+          min={0}
+          value={productPriceInput}
+          onChange={(e) => setProductPriceInput(e.target.value)}
+        />
+        <input
+          required
+          placeholder="Código do produto"
+          className="border px-4 py-2 rounded-lg"
+          type="text"
+          value={productIdInput}
+          onChange={(e) => setProductIdInput(e.target.value)}
+        />
+      </div>
+
+      <button disabled={isLoading} className="border px-4 py-2 rounded-lg" type="submit">
+        {isLoading ? <span className="animate-pulse">Carregando</span> : <span>Criar</span>}
+      </button>
+    </form>
+  )
+}
